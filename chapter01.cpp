c0205b5b1d831bf570b59e8bd9cf7d0e20ec0d1f
Cecil <csx17@qq.com>
2019-11-23 14:52:52 +0800

    完成内容

--- chapter01.cpp
+++ chapter01.cpp
@@ -37,7 +37,7 @@ public:
 
     //获取算法选择输入
     void InputArith() {
-        cout << "请选择想要先使用的算法（1-FCFS，2-SJF)：";
+        cout << endl << "请选择想要先使用的算法（1-FCFS，2-SJF)：";
         cin >> isArith;
         IsArith();
     }
@@ -45,10 +45,10 @@ public:
     //算法存储
     void IsArith() {
         if (isArith == 1) {
-            cout << "您选择的是1-FCFS算法" << endl;
+            cout << endl << "您选择的是1-FCFS算法" << endl;
             arith = FCFS;
         } else if (isArith == 2) {
-            cout << "您选择的是2-SJF算法" << endl;
+            cout << endl << "您选择的是2-SJF算法" << endl;
             arith = SJF;
         } else {
             cout << "算法值" << isArith << "有误,请重新输入正确的算法类型（1-FCFS，2-SJF）" << endl;
@@ -85,32 +85,79 @@ public:
     //调用SJF算法进行调度计算
     void ArithSJF() {
 
-        //初始化FinishTime[0]
+        int QueueServiceTime[MaxNum];//排序服务时间
+        int CopyServiceTime[MaxNum];//备份服务时间
+
+        //初始化QueueServiceTime
+        for (int i = 1; i <= n; i++) {
+            QueueServiceTime[i] = ServiceTime[i];
+        }
+
+        //初始化CopyServiceTime
+        for (int i = 1; i <= n; i++) {
+            CopyServiceTime[i] = ServiceTime[i];
+        }
+
+        //给FinishTime[0]附初值
         FinishTime[0] = 0;
 
-        int temp[n + 1];//进程服务时间S[i]
-
-        //从小到大冒泡排序
+        //从小到大冒泡排序QueueServiceTime
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < n - i; j++) {
-                if (ServiceTime[j] > ServiceTime[j + 1]) {
-                    int temp = nums[j];
-                    temp[j] = nums[j + 1];
-                    temp[j + 1] = temp;
+            for (int j = 1; j <= n - i; j++) {
+                if (QueueServiceTime[j] > QueueServiceTime[j + 1]) {
+                    int temp = QueueServiceTime[j];
+                    QueueServiceTime[j] = QueueServiceTime[j + 1];
+                    QueueServiceTime[j + 1] = temp;
                 }
             }
         }
 
+        //计算完成时间
+        int temp = -1, old_process = 0, new_process = 0;//定义暂存值、上一个进程、本进程
+
         for (int i = 1; i <= n; i++) {
 
-            //计算完成时间
-            FinishTime[i] = FinishTime[i - 1] + ServiceTime[i];
+            old_process = new_process;
+
+            //查找服务时间最短的进程
+            if (i != 1) {
+                for (int j = 1; j <= n; j++) {
+                    if (QueueServiceTime[j] > 0 && ArrivalTime[j] <= FinishTime[old_process] + 1) {
+                        temp = QueueServiceTime[j];
+                        QueueServiceTime[j] = -1;
+                        break;
+                    }
+                }
+            } else//若为第1个进程，直接进入队列，完成时间即为服务时间
+            {
+                for (int j = 1; j <= n; j++) {
+                    if (QueueServiceTime[j] == ServiceTime[i]) {
+                        temp = QueueServiceTime[j];
+                        QueueServiceTime[j] = -1;
+                        break;
+                    }
+                }
+            }
+
+            //查找该最短服务时间对应的进程号
+            for (int j = 1; j <= n; j++) {
+                if (CopyServiceTime[j] == temp && CopyServiceTime[j] > 0) {
+                    new_process = j;
+                    CopyServiceTime[j] = -1;
+                    break;
+                }
+            }
+
+            //计算存储完成时间
+            if (i != 1) {
+                FinishTime[new_process] = FinishTime[old_process] + temp;
+            } else {
+                FinishTime[i] = ArrivalTime[i] + ServiceTime[i];
+            }
 
         }
-/*        for (int i = 1; i <= n; i++) {
-            //计算完成时间
-            FinishTime[i] = FinishTime[i - 1] + ServiceTime[i];
 
+        for (int i = 1; i <= n; i++) {
             //计算周转时间
             WholeTime[i] = FinishTime[i] - ArrivalTime[i];
 
@@ -122,17 +169,51 @@ public:
 
             //计算平局带权周转时间
             AverageWWT_FCFS += WeightWholeTime[i];
-        }*/
+        }
+        cout << endl;
+
         AverageWT_FCFS = (double) AverageWT_FCFS / n;
         AverageWWT_FCFS = (double) AverageWWT_FCFS / n;
     }
 
     //输出调度过程
     void PrintAttemper() {
+        int temp = 0;
+        int CopyFinishTime[MaxNum];//备份服务时间
+
+        //初始化CopyServiceTime
+        for (int i = 1; i <= n; i++) {
+            CopyFinishTime[i] = FinishTime[i];
+        }
+
         if (FinishTime[1] >= 0)//判断是否已有数据输入
+            //根据完成时间排序输出
             for (int i = 1; i <= n; i++) {
+
                 if (i != 1) {
-                    cout << "时刻" << FinishTime[i - 1] + 1 << "进程" << i << "开始运行" << endl;
+                    for (int j = 1; j <= n; j++) {
+                        if (temp != 0) {
+                            if (CopyFinishTime[j] > 0) {
+                                temp = (CopyFinishTime[temp] < CopyFinishTime[j] ? temp : j);
+                            }
+                        } else {
+                            if (CopyFinishTime[j] > 0) {
+                                temp = j;
+                            }
+                        }
+                    }
+                    if (i != 2) { cout << "进程" << temp << "开始运行" << endl; }
+                    cout << "时刻" << FinishTime[temp] + 1;
+                    if (i == n) {
+                        for (int i = 1; i < n; i++) {
+                            if (CopyFinishTime[i] > 0) {
+                                temp = i;
+                            }
+                        }
+                        cout << "进程" << temp << "开始运行" << endl << endl;
+                    }
+                    CopyFinishTime[temp] = -1;
+                    temp = 0;
                 } else {
                     cout << "时刻0进程" << i << "开始运行" << endl;
                 }
@@ -218,18 +299,6 @@ public:
             cout << "平均带权周转时间:" << AverageWWT_FCFS << endl;
         }
     }
-
-    //输出测试
-    void TestOut() {
-        cout << "完成时间    ";
-        for (int i = 1; i <= n; i++) {
-            if (0 <= FinishTime[i] && FinishTime[i] < 10) {
-                cout << "  " << FinishTime[i] << " ";
-            }
-
-        }
-        cout << endl;
-    }
 };
 
 
@@ -257,6 +326,4 @@ int main() {
 
     //输出周转时间、带权周转时间、平均周转时间及带权平均周转时间
     timeCal.Print();
-
-    //timeCal.TestOut();
 }
